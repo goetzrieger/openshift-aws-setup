e6cac861ee94b3318c7e7e17e3ff3cb49e7a46fd
gnunn1 <gerald.b.nunn@gmail.com>
2018-08-02 19:50:49 -0400

    Initial work on installing 3.10

--- roles/openshift-install/files/install-openshift.sh
+++ roles/openshift-install/files/install-openshift.sh
@@ -7,7 +7,7 @@ if [ $1 = "origin" ]; then
   OA_PREFIX=/home/{{amazon_user}}
 fi
 
-{% if ocp_version|float <= 3.7  %}
+{% if ocp_version|version_compare('3.7', '<=')  %}
 ansible-playbook -v -i ~{{amazon_user}}/openshift_inventory.cfg ${OA_PREFIX}/openshift-ansible/playbooks/byo/config.yml
 {% else %}
 ansible-playbook -v -i ~{{amazon_user}}/openshift_inventory.cfg ${OA_PREFIX}/openshift-ansible/playbooks/prerequisites.yml
